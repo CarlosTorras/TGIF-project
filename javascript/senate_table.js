562f21c33caf7327959b6d788094d4913907a421
Carlos Torras <carlos6torras@gmail.com>
2019-08-06 15:42:11 +0200

    add url links to table names

--- javascript/senate_table.js
+++ javascript/senate_table.js
@@ -11,11 +11,12 @@ function printtable(array, id) {
     let statecell = document.createElement("td");
     let senioritycell = document.createElement("td");
     let totalvotescell = document.createElement("td");
-
-    fullnamecell.innerHTML = array[i].last_name + " " + array[i].first_name;
+    var fullname = array[i].last_name + " " + array[i].first_name;
     if (array[i].middle_name !== null) {
       fullnamecell.innerHTML += " " + array[i].middle_name;
     }
+
+    fullnamecell.innerHTML = fullname.link(array[i].url);
     partycell.innerHTML = array[i].party;
     statecell.innerHTML = array[i].state;
     senioritycell.innerHTML = array[i].seniority;
