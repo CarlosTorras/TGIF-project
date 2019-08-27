ab5b4a12ce1e34ccf7814d798e4d9d1ffff99a66
Carlos Torras <carlos6torras@gmail.com>
2019-08-27 16:33:23 +0200

    Implement filters (checkboxes and dropdown)

--- javascript/senate_table.js
+++ javascript/senate_table.js
@@ -32,3 +32,66 @@ function printtable(array, id) {
     tbody.append(row);
   }
 }
+
+// Filter for party
+
+document.getElementById("demparty").addEventListener("click", function() {
+  partyfilter("senate-data");
+});
+document.getElementById("repparty").addEventListener("click", function() {
+  partyfilter("senate-data");
+});
+document.getElementById("indparty").addEventListener("click", function() {
+  partyfilter("senate-data");
+});
+
+function partyfilter(id) {
+  var table = document.getElementById(id);
+  var trrow = table.getElementsByTagName("tr");
+  var demcheck = document.getElementById("demparty").checked;
+  var repcheck = document.getElementById("repparty").checked;
+  var indcheck = document.getElementById("indparty").checked;
+
+  for (i = 0; i < trrow.length; i++) {
+    var tdparty = trrow[i].getElementsByTagName("td")[1].innerText;
+
+    trrow[i].style.display = "none";
+
+    if (demcheck == false && repcheck == false && indcheck == false) {
+      trrow[i].style.display = "";
+    }
+
+    if (tdparty == "D" && demcheck == true) {
+      trrow[i].style.display = "";
+    }
+
+    if (tdparty == "R" && repcheck == true) {
+      trrow[i].style.display = "";
+    }
+
+    if (tdparty == "I" && indcheck == true) {
+      trrow[i].style.display = "";
+    }
+  }
+}
+
+//Filter for state
+
+document.getElementById("ddownstate").addEventListener("change", function() {
+  statefilter("senate-data");
+});
+
+function statefilter(id) {
+  var table = document.getElementById(id);
+  var trrow = table.getElementsByTagName("tr");
+  var value = document.getElementById("ddownstate").value;
+  console.log(value);
+
+  for (i = 0; i < trrow.length; i++) {
+    var tdparty = trrow[i].getElementsByTagName("td")[2].innerText;
+    trrow[i].style.display = "none";
+    if (tdparty == value) {
+      trrow[i].style.display = "";
+    }
+  }
+}
